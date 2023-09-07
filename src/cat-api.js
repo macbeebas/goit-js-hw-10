1a2c58cd1479b0fe6506cc96aeaf5ef3fa5d7a9f
Michał Viwegier <beebas@wp.pl>
2023-09-08 00:53:57 +0200

    Refactor changes - function moved to 'index.js'. In 'cat-api.js' only 'fetch' functions are left.

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,10 +5,8 @@ axios.defaults.headers.common['x-api-key'] =
 
 import Notiflix from 'notiflix';
 
-export const breedSelect = document.querySelector('.breed-select');
 export const pLoader = document.querySelector('.loader');
 export const pError = document.querySelector('.error');
-export const catInfo = document.querySelector('.cat-info');
 
 export function fetchBreeds() {
   return axios
@@ -18,37 +16,20 @@ export function fetchBreeds() {
       Notiflix.Notify.failure(
         'Oops! Something went wrong! Try reloading the page!'
       );
-      pError.classList.remove('hidden');
+      pError.classList.remove('invisible');
+      pLoader.classList.add('invisible');
     });
 }
 
-export function buildCatElement(item) {
-  var altName = item.breeds[0].alt_names;
-  if (altName === undefined || altName === '' || altName.length <= 1) {
-    altName = `${item.breeds[0].name} cat`;
-  }
-  const tempCat = `<img class="cat-info__image" src="${item.url}" alt="${altName}" width="400px" />
-<h1 class="cat-info__header">${item.breeds[0].name}</h1>
-<p class="cat-info__description">${item.breeds[0].description}</p>
-<p class="cat-info__temperament"><strong>Temperament: </strong><span>${item.breeds[0].temperament}</span></p>`;
-  catInfo.innerHTML = tempCat;
-}
-
 export function fetchCatByBreed(breedId) {
-  axios
+  return axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => response.data)
-    .then(data => {
-      buildCatElement(data[0]);
-      pError.classList.add('invisible');
-    })
     .catch(error => {
       Notiflix.Notify.failure(
         'Oops! Something went wrong! Try reloading the page!'
       );
       pError.classList.remove('invisible');
-    })
-    .finally(() => {
       pLoader.classList.add('invisible');
     });
 }
--- src/index.js
+++ src/index.js
@@ -9,18 +9,20 @@ import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
 
 import {
-  fetchBreeds, // func
-  fetchCatByBreed, //func
-  breedSelect, // var
   pLoader, // var
   pError, //var
-  catInfo, // var
+  fetchBreeds, // func
+  fetchCatByBreed, //func
 } from './cat-api';
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 
 breedSelect.setAttribute('id', 'selectElement');
 pLoader.innerHTML = '';
 breedSelect.classList.add('invisible');
 pError.classList.add('invisible');
+pLoader.classList.add('invisible');
 
 function createSelectionList(data) {
   if (!data) {
@@ -35,6 +37,22 @@ function createSelectionList(data) {
   breedSelect.insertAdjacentHTML('beforeend', flatArrayWithCats);
 }
 
+function buildCatElement(item) {
+  var altName = item[0].breeds[0].alt_names;
+  if (altName === undefined || altName === '' || altName.length <= 1) {
+    altName = `${item[0].breeds[0].name} cat`;
+  }
+  const urlItem = item[0].url;
+  const nameItem = item[0].breeds[0].name;
+  const descrItem = item[0].breeds[0].description;
+  const temperItem = item[0].breeds[0].temperament;
+  const tempCat = `<img class="cat-info__image" src="${urlItem}" alt="${altName}" width="400px" />
+<h1 class="cat-info__header">${nameItem}</h1>
+<p class="cat-info__description">${descrItem}</p>
+<p class="cat-info__temperament"><strong>Temperament: </strong><span>${temperItem}</span></p>`;
+  catInfo.innerHTML = tempCat;
+}
+
 fetchBreeds()
   .then(value => {
     createSelectionList(value);
@@ -42,6 +60,7 @@ fetchBreeds()
   })
   .catch(error => {
     pError.classList.remove('invisible');
+    pLoader.classList.add('invisible');
   })
   .finally(() => {
     breedSelect.classList.remove('invisible');
@@ -56,16 +75,18 @@ breedSelect.addEventListener('change', selectedCatItem);
 
 function selectedCatItem(event) {
   pLoader.classList.remove('invisible');
-  fetchCatByBreed(event.target.selectedOptions[0].value)
+  const shortCat = event.target.selectedOptions[0].value;
+  fetchCatByBreed(shortCat)
     .then(response => {
+      buildCatElement(response);
       pError.classList.add('invisible');
-      return response;
+      pLoader.classList.remove('invisible');
     })
     .catch(error => {
       Notiflix.Notify.failure(
         'Oops! Something went wrong! Try reloading the page!'
       );
-      pError.classList.remove('hidden');
+      pError.classList.remove('invisible');
     })
     .finally(() => {
       pLoader.classList.add('invisible');
